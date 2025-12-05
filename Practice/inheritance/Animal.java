6d176ee79e1d48b12fcd6330244b3e2d9766d946
buipd <buipd@rose-hulman.edu>
2025-12-04 22:18:30 -0500

    there are bugs somewhere

---
+++ Practice/inheritance/Animal.java
@@ -0,0 +1,14 @@
+package Practice.inheritance;
+
+public class Animal {
+    
+    boolean isAlive;
+
+    Animal(){
+        isAlive = true;
+    }
+
+    void eat(){
+        System.out.println("The animal is eating");
+    }
+}
---
+++ Practice/inheritance/out.java
@@ -0,0 +1,9 @@
+package Practice.inheritance;
+
+public class out {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        System.out.println(dog.isAlive);
+        dog.eat();
+    }
+}
